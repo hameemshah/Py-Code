fd43acaff2a4d7c83c3b4e4f5c4b5755cea82003
Hameem Hussain Shah <muhammad.hameem.shah@gmail.com>
2022-12-04 10:04:33 +0530

    Edited

--- card_game.py
+++ card_game.py
@@ -1,4 +1,5 @@
 import random
+
 #Funcion to give the final result
 def result(result_value):
     if value == 1:
@@ -7,6 +8,7 @@ def result(result_value):
         print("You Lose")
     else:
         print("Draw")
+
 #Function to compare the result between user and computer
 def compare(user,computer):
     if sum(user)>21:
@@ -34,27 +36,34 @@ def compare(user,computer):
         return 2
     else:
         return("something went wrong")
+
 #Function to deal between the deck of cards
 def deal(player):
     cards = [11,2,3,4,5,6,7,8,9,10,10,10]
     player.append(random.choice(cards))
+
 #If sum is greater than 20 the 11 becomes 1
     if sum(player)>20 and 11 in player:
         player[player.index(11)]=1
+    
 #Creating an empty user list to store card values
 user =[]
+
 #Getting two cards for user form the deck of cards in deal function
 for _ in range(2):
     deal(user)
 print(f"Your cards are {user}")
+
 #Checking whether it is black jack to stop execution further
 if sum(user)==21:
     print("BlackJack! You Won")
     exit()
+    
 #Creating empty list for computer 
 computer = []
 deal(computer)
 print(f"The card of computer is {computer}")
+
 #Giving user a choice to choose extra cards whose value is not exceeding 21
 flag = 'y'
 while flag=='y' and sum(user)<21:
@@ -63,10 +72,13 @@ while flag=='y' and sum(user)<21:
         deal(user)
     print(f"Your choices are {user}")
 deal(computer)
+
 #If after seconds deals the computer has less than 16 sum, then giving it an extra deal
 if sum(computer)<16:
     deal(computer)
+    
 #Finally calling the compare function to check the winner
 value = (compare(user,computer))
+
 #Printing winner and loser using result function
 result(value)
--- cipher.py
+++ cipher.py
@@ -1,24 +1,25 @@
-def encode(message,shift):
-    encoded_message = []; cipher = [];
-    for i in range(0,len(message)):
-        encoded_message.append(ord(message[i])+int(shift))
-        cipher.append(chr(encoded_message[i]))
+def encode(msg,sft):
+    cipher = [];
+    for char in msg:
+        cipher.append(chr(ord(char)+int(sft)))
     return cipher
-def decode(message,shift):
-    decoded_message = ord(message)-int(shift)
-    return chr(decoded_message)
+
+def decode(msg,sft):
+    decipher = [];
+    for char in msg:
+        decipher.append(chr(ord(char)-int(sft)))
+    return  decipher
+
 print("Welcome to cipher")
 task = input("Enter E for encoding and D for decoding:").lower()
-char_list = []
+
 if(task == 'e'):
     message = input("Enter the message:")
-    shift = input("Enter the shift")
-    for i in message:
-        char_list.append(i)
-    print(encode(char_list,shift))
+    shift = input("Enter the shift:")
+    print("".join(encode(message,shift)))
 elif(task == 'd'):
-    message = input("Enter the message and shift:")
-    shift = input("Enter the shift")
-    print(decode(message,shift))
+    message = input("Enter the message:")
+    shift = input("Enter the shift:")
+    print("".join(decode(message,shift)))
 else:
     print("Wrong Input")
